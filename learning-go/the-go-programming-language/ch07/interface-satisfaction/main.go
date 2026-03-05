d0ac48b29802a56fd8e7bc014d5fd804d5d57e18
Richard Lechko <rlechko@hendrickson-intl.com>
2026-03-05 17:08:08 -0600

    added more code

--- learning-go/the-go-programming-language/ch07/interface-satisfaction/main.go
+++ learning-go/the-go-programming-language/ch07/interface-satisfaction/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"os"
-	"time"
 )
 
 func main() {
 	var w io.Writer
 	w = os.Stdout
 	w = new(bytes.Buffer)
-	w = time.Second
+	// w = time.Second // compile error, time.Duration does not implement io.Writer
 
 	var rwc io.ReadWriteCloser
 	rwc = os.Stdout
-	rwc = new(bytes.Buffer)
+	// rwc = new(bytes.Buffer) // compile error, bytes.Buffer does not implement io.ReadWriteCloser
+	fmt.Printf("%v & %v\n", w, rwc)
+
+	var any interface{}
+	any = true
+	any = 12.34
+	any = "hello"
+	any = map[string]int{"one": 1}
+	any = new(bytes.Buffer)
 }
